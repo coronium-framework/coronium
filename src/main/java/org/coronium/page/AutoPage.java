4f462f4f6e1253d4568b10bb1819152fab576289
dazubairy <dazubairy@silversky.com>
2020-08-12 14:53:28 +0800

    apple test

--- src/main/java/org/coronium/page/AutoPage.java
+++ src/main/java/org/coronium/page/AutoPage.java
@@ -1,8 +1,44 @@
 package org.coronium.page;
 
 import org.coronium.test.AutoTest;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Wait;
+import ru.yandex.qatools.htmlelements.loader.HtmlElementLoader;
+import smc.page.core.ui.js.JavascriptWait;
+import smc.page.core.ui.pages.Visibility;
 
 public class AutoPage {
+
+    protected final WebDriver driver;
+    protected Wait<WebDriver> wait;
+    protected Visibility visibility;
+    protected JavascriptWait javascriptWait;
+
+    public AutoPage() {
+        driver = AutoTest.getDriver();
+        wait = AutoTest.getWait();
+        visibility = new Visibility(wait, (JavascriptExecutor) AutoTest.getDriver());
+    }
+
     public static WebDriver getDriver() { return AutoTest.getDriver(); }
+
+    public AutoPage get(long timeoutInSeconds) {
+        updatePageTimeout(timeoutInSeconds);
+        return get();
+    }
+
+    private AutoPage get(){
+        HtmlElementLoader.populatePageObject(this,getDriver());
+        visibility.waitForAnnotatedElementVisibility(this);
+        javascriptWait.waitForJavascriptEventsOnLoad();
+
+        return (AutoPage) this;
+    }
+
+    private void updatePageTimeout(long timeoutInSeconds) {
+        wait = AutoTest.newWaitWithTimeout(timeoutInSeconds);
+        visibility = new Visibility(wait, (JavascriptExecutor) AutoTest.getDriver());
+        javascriptWait = new JavascriptWait(driver, wait);
+    }
 }
--- src/main/java/org/coronium/test/AutoTest.java
+++ src/main/java/org/coronium/test/AutoTest.java
@@ -3,15 +3,21 @@ package org.coronium.test;
 import org.coronium.util.DriverManager;
 import org.coronium.util.DriverManagerFactory;
 import org.coronium.util.DriverType;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+import java.time.Duration;
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 public abstract class AutoTest {
     protected static String environment = System.getProperty("environment");
     protected static WebDriver driver = null;
+    private static final ThreadLocal<Wait<WebDriver>> wait = ThreadLocal.withInitial(() -> null);
 
     @BeforeClass
     public void beforeClass() {
@@ -34,7 +40,18 @@ public abstract class AutoTest {
         }
     }
 
+    public static Wait<WebDriver> newWaitWithTimeout(long timeout){
+        return new FluentWait<WebDriver>(driver)
+                .withTimeout(Duration.ofDays(timeout)) //if breaks,change here
+                .ignoring(NoSuchElementException.class)
+                .ignoring(StaleElementReferenceException.class);
+    }
+
     public static WebDriver getDriver() {
         return driver;
+    }
+
+    public static Wait<WebDriver> getWait() {
+        return wait.get(); //need to update
     }
 }
