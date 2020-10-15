74a1e9142111528abfc481bf79bc9166d8ab80f9
coronium-framework <dazubairy@silversky.com>
2020-10-15 16:13:29 +0800

    Removed user page - added new wait in Wait.java - update ExtraExpectedConditions.java

--- src/main/java/org/coronium/util/helper/ExtraExpectedConditions.java
+++ src/main/java/org/coronium/util/helper/ExtraExpectedConditions.java
@@ -8,42 +8,97 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import java.util.List;
 import java.util.function.Function;
 
-import static org.coronium.util.constant.Locator.MODAL_CSS;
-import static org.coronium.util.constant.Locator.SPINNER_CSS;
+import static org.coronium.util.constant.Locator.*;
 
 public class ExtraExpectedConditions {
-    public static ExpectedCondition<Boolean> invisibilityOfSpinner(){
+    public static ExpectedCondition<Boolean> invisibilityOfSpinner() {
         return expectedCondition(
-            getDriver -> {
-                List<WebElement> elements = getDriver.findElements(By.cssSelector(SPINNER_CSS));
-                return elements.size() <= 0 ||
-                        elements.parallelStream()
-                        .noneMatch(WebElement::isDisplayed);
-            },
-                "Spinnner not to be present or visible");
+                driver -> {
+                    List<WebElement> elements = driver.findElements(By.cssSelector(SPINNER_CSS));
+                    return elements.size() <= 0 ||
+                            elements.parallelStream().noneMatch(WebElement::isDisplayed);
+                },
+                "Spinner to not be present or visible"
+        );
     }
 
-    public static ExpectedCondition<Boolean> invisibilityOfModal(){
+    public static ExpectedCondition<Boolean> visibilityOfSpinner() {
         return expectedCondition(
-                getDriver ->{
-                    List<WebElement> elements = getDriver.findElements(By.cssSelector(MODAL_CSS));
-                    return elements.size() <=0 ||
+                driver -> {
+                    List<WebElement> elements = driver.findElements(By.cssSelector(SPINNER_CSS));
+                    return elements.size() > 0 &&
+                            elements.parallelStream().anyMatch(WebElement::isDisplayed);
+                },
+                "Spinner to  be present or visible"
+        );
+    }
+
+    public static ExpectedCondition<Boolean> invisibilityOfModal() {
+        return expectedCondition(
+                driver -> {
+                    List<WebElement> elements = driver.findElements(By.cssSelector(MODAL_CSS));
+                    return elements.size() <= 0 ||
                             elements.parallelStream().noneMatch(WebElement::isDisplayed);
                 },
                 "Modal to not be present or visible"
         );
     }
 
-            private static <T> ExpectedCondition<T> expectedCondition(Function< WebDriver, T> function, String string){
-                return new ExpectedCondition<T>() {
-                    @Override
-                    public T apply(WebDriver driver) {
-                        return function.apply(driver);
-                    }
+    public static ExpectedCondition<Boolean> visibilityOfModal() {
+        return expectedCondition(
+                driver -> {
+                    List<WebElement> elements = driver.findElements(By.cssSelector(MODAL_CSS));
+                    return elements.size() > 0 &&
+                            elements.parallelStream().anyMatch(WebElement::isDisplayed);
+                },
+                "Modal to not be present or visible"
+        );
+    }
 
-                    @Override
-                    public String toString(){return string; }
-                };
-        }
+    private static <T> ExpectedCondition<T> expectedCondition(Function<WebDriver, T> function, String string) {
+        return new ExpectedCondition<T>() {
+            @Override
+            public T apply(WebDriver webDriver) {
+                return function.apply(webDriver);
+            }
 
+            @Override
+            public String toString() {
+                return string;
+            }
+        };
+    }
+
+    public static ExpectedCondition<Boolean> visibilityOfStyleDisplayBlock() {
+        return expectedCondition(
+                driver -> {
+                    List<WebElement> elements = driver.findElements(By.cssSelector(STYLE_DISPLAY_BLOCK_CSS));
+                    return elements.size() > 0 ||
+                            elements.parallelStream().anyMatch(WebElement::isDisplayed);
+                },
+                "Style display block to not be present or visible"
+        );
+    }
+
+    public static ExpectedCondition<Boolean> invisibilityOfStyleDisplayBlock() {
+        return expectedCondition(
+                driver -> {
+                    List<WebElement> elements = driver.findElements(By.cssSelector(STYLE_DISPLAY_BLOCK_CSS));
+                    return elements.size() <= 0 ||
+                            elements.parallelStream().noneMatch(WebElement::isDisplayed);
+                },
+                "Style display block to not be present or visible"
+        );
+    }
+
+    public static ExpectedCondition<Boolean> invisibilityOfWaitingBar() {
+        return expectedCondition(
+                driver -> {
+                    List<WebElement> elements = driver.findElements(By.cssSelector(WAITING_BAR_CSS));
+                    return elements.size() <= 0 ||
+                            elements.parallelStream().noneMatch(WebElement::isDisplayed);
+                },
+                "Spinner to not be present or visible"
+        );
+    }
 }
--- src/main/java/org/coronium/util/helper/Wait.java
+++ src/main/java/org/coronium/util/helper/Wait.java
@@ -1,24 +1,98 @@
 package org.coronium.util.helper;
 
 import org.coronium.test.AutoTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import ru.yandex.qatools.htmlelements.element.Table;
+import ru.yandex.qatools.htmlelements.element.TypifiedElement;
 
-import static org.coronium.util.constant.Time.GLOBAL_TABLE_LOAD_TIMEOUT;
-import static org.coronium.util.constant.Time.GLOBAL_WAIT_LOADING_TIMEOUT;
+import static org.coronium.util.constant.Time.*;
 
 public class Wait {
     private static WebDriver driver;
-    public static void waitForInvisibilityOfSpinner() {
-        AutoTest.newWaitWithTimeout(GLOBAL_TABLE_LOAD_TIMEOUT.seconds())
-                .until(ExtraExpectedConditions.invisibilityOfSpinner());
+    public static void waitForModalBlock() {
+        try {
+            AutoTest.newWaitWithTimeout(GLOBAL_PAGE_LOAD_TIMEOUT.seconds()).until(ExtraExpectedConditions.invisibilityOfModal());
+        } catch (org.openqa.selenium.TimeoutException exception) {
+            //ignore
+        }
+    }
+
+    public static void waitForDisplayUnblock() {
+        try {
+            AutoTest.newWaitWithTimeout(GLOBAL_TABLE_LOAD_TIMEOUT.seconds()).until(ExtraExpectedConditions.invisibilityOfStyleDisplayBlock());
+        } catch (org.openqa.selenium.TimeoutException exception) {
+            //ignore
+        }
+    }
+
+    public static void waitForDisplayUnblockQuick() {
+        try {
+            AutoTest.newWaitWithTimeout(GLOBAL_TABLE_LOAD_QUICK_TIMEOUT.seconds()).until(ExtraExpectedConditions.invisibilityOfStyleDisplayBlock());
+        } catch (org.openqa.selenium.TimeoutException exception) {
+            //ignore
+        }
+    }
+
+    public static void waitForDisplayBlockUnblock() {
+        try {
+            AutoTest.newWaitWithTimeout(GLOBAL_TABLE_LOAD_TIMEOUT.seconds()).until(ExtraExpectedConditions.visibilityOfStyleDisplayBlock());
+            AutoTest.newWaitWithTimeout(GLOBAL_TABLE_LOAD_TIMEOUT.seconds()).until(ExtraExpectedConditions.invisibilityOfStyleDisplayBlock());
+        } catch (org.openqa.selenium.TimeoutException exception) {
+            //ignore
+        }
+    }
+
+    public static void waitForSpinnerInside(Table table) {
+        try {
+            AutoTest.newWaitWithTimeout(GLOBAL_TABLE_LOAD_TIMEOUT.seconds())
+                    .until(ExpectedConditions.invisibilityOf(table.findElement(By.cssSelector("div[style='display: block;']"))));
+        } catch (NoSuchElementException e) {
+            //ignore
+        }
+    }
+
+    public static void waitForSpinnerInside(TypifiedElement typifiedElement) {
+        try {
+            AutoTest.newWaitWithTimeout(GLOBAL_TABLE_LOAD_TIMEOUT.seconds())
+                    .until(ExpectedConditions.invisibilityOf(typifiedElement.findElement(By.cssSelector("div[style='display: block;']"))));
+        } catch (NoSuchElementException e) {
+            //ignore
+        }
     }
 
     public static void waitForVisibilityOf(WebElement webElement) {
         try {
             AutoTest.newWaitWithTimeout(GLOBAL_WAIT_LOADING_TIMEOUT.seconds())
                     .until(ExpectedConditions.visibilityOf(webElement));
+        } catch (org.openqa.selenium.TimeoutException exception) {
+            //ignore
+        }
+    }
+
+    public static void waitForInvisibilityOf(WebElement webElement) {
+        AutoTest.newWaitWithTimeout(GLOBAL_PAGE_LOAD_TIMEOUT.seconds())
+                .until(ExpectedConditions.invisibilityOf(webElement));
+    }
+
+    public static void waitForInvisibilityOfSpinner() {
+        AutoTest.newWaitWithTimeout(GLOBAL_TABLE_LOAD_TIMEOUT.seconds())
+                .until(ExtraExpectedConditions
+                        .invisibilityOfSpinner());
+    }
+
+    public static void waitForElementToBeClickable(WebElement element) {
+        AutoTest.newWaitWithTimeout(GLOBAL_PAGE_LOAD_TIMEOUT.seconds())
+                .until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public static void waitForTableLoad() {
+        try {
+            AutoTest.newWaitWithTimeout(100)
+                    .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("./ancestor-or-self::table")));
         } catch (org.openqa.selenium.TimeoutException exception) {
             //ignore
         }
@@ -33,6 +107,7 @@ public class Wait {
         }
     }
 
+    // LAST RESORT ONLY
     public static void waitExplicitly(int seconds) {
         try {
             Thread.sleep(seconds * 1000);
