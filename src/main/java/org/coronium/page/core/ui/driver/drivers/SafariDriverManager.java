6227607cbfc59be0bd619ccf073f7804637ccf8c
coronium-framework <dazubairy@silversky.com>
2020-11-10 17:10:34 +0800

    Added Safari Driver

--- src/main/java/org/coronium/page/core/ui/driver/drivers/SafariDriverManager.java
+++ src/main/java/org/coronium/page/core/ui/driver/drivers/SafariDriverManager.java
@@ -1,14 +1,31 @@
 package org.coronium.page.core.ui.driver.drivers;
 
+import org.coronium.page.core.ui.driver.Driver;
 import org.coronium.page.core.ui.driver.DriverManager;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.safari.SafariOptions;
 
 public class SafariDriverManager extends DriverManager {
 
     @Override
     protected void createWebDriver() {
-        SafariOptions options = new SafariOptions();
-        this.driver = (WebDriver) new SafariOptions(options);
+        if (Driver.isMobile()) {
+            //code here
+        } else {
+            SafariOptions safariOptions = new SafariOptions();
+            safariOptions.setCapability("safari.cleanSession", true);
+
+        }
     }
+        public WebDriver getWebDriver(Capabilities capabilities) {
+            if (Driver.isMobile()) {
+                throw new IllegalArgumentException(
+                        "seleniumGridURL or sauceUser and sauceKey must be specified when running on iOS");
+            } else {
+                return new SafariDriver(new SafariOptions(capabilities));
+            }
+        }
+
 }
